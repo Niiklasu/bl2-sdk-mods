e17b76fe434123bad5e8e6b8e7ba6c070e8d0062
Niiklasu <unrau73.wu@gmail.com>
2025-02-10 23:14:26 +0100

    wokrking coop + some more drawing options

--- DamageMeter/__init__.py
+++ DamageMeter/__init__.py
@@ -1,31 +1,34 @@
 from __future__ import annotations
 from enum import Enum
-from typing import TYPE_CHECKING, TypedDict, cast
+from typing import TYPE_CHECKING, ClassVar, TypedDict, cast
 from DamageMeter import drawing
+from coroutines import start_coroutine_post_render, WaitForSeconds, PostRenderCoroutine, WaitUntil
 from mods_base.keybinds import keybind
 from mods_base.mod import CoopSupport, Game
 from networking.decorators import broadcast
 from networking.factory import add_network_functions
-from unrealsdk import find_enum
-from mods_base import ENGINE, hook, build_mod, options
+from unrealsdk import find_enum, find_object
+from mods_base import ENGINE, get_pc, hook, build_mod, options
 from unrealsdk.unreal import BoundFunction
 
 if TYPE_CHECKING:
-    from bl2 import WillowGameEngine, WillowPlayerController, WillowPawn, WorldInfo, WillowGame, Canvas
-
-TITLE = "Damage Meter"
+    from bl2 import WillowGameEngine, WillowPlayerController, WillowPawn, WorldInfo, Canvas, FrontendGFxMovie
 
 
 def on_default_active_change(_option: options.BoolOption, value: bool) -> None:
-    DamageMeterState.is_logging = value
+    if value and DamageMeterState.is_hidden:
+        DamageMeterState.is_hidden = False
+        DamageMeterState.is_paused = False
 
 
 defaultActive = options.BoolOption(
-    "DefaultActive",
-    False,
+    identifier="Active by Default",
+    value=False,
     description="Whether the mod should be active by default",
     on_change=on_default_active_change,
 )
+
+TITLE = "Damage Meter"
 
 
 # single source of truth for character name from game -> enum
@@ -60,19 +63,28 @@ class PlayerStats(TypedDict):
 
 
 class DamageMeterState:
-    is_logging = defaultActive.value
-    player_stats: dict[str, PlayerStats] = {}
+    is_hidden: ClassVar[bool] = True
+    is_paused: ClassVar[bool] = True
+    player_stats: ClassVar[dict[str, PlayerStats]] = {}
 
 
-# from slide mod by @juso40
-def is_client() -> bool:
-    e_net_mode: WorldInfo.ENetMode = cast("WorldInfo.ENetMode", find_enum("ENetMode"))
-    return cast("WillowGameEngine", ENGINE).GetCurrentWorldInfo().NetMode == e_net_mode.NM_Client
-
-
-@broadcast.json_message
-def store_stats(stats: dict[str, PlayerStats]) -> None:
-    DamageMeterState.player_stats = stats
+@hook("WillowGame.WillowPlayerController:ShouldLoadSaveGameOnSpawn")
+def on_spawn(
+    pc: WillowPlayerController,
+    args: WillowPlayerController._ShouldLoadSaveGameOnSpawn.args,
+    _ret: WillowPlayerController._ShouldLoadSaveGameOnSpawn.ret,
+    _func: BoundFunction,
+) -> None:
+    # have to check if hook is called client side, for now we always ignore client side
+    if is_client():
+        return
+    if not args.bIsInitialSpawn:
+        return
+    DamageMeterState.player_stats[pc.PlayerReplicationInfo.PlayerName] = {
+        "damage": 0,
+        # "tanked_damage": 0,
+        "character_class": CharacterClass(pc.PlayerClass.CharacterNameId.CharacterName),
+    }
 
 
 @hook("WillowGame.WillowPawn:TookDamageFromEnemy")
@@ -82,11 +94,9 @@ def took_damage_from_enemy(
     __ret: WillowPawn._TookDamageFromEnemy.ret,
     __func: BoundFunction,
 ) -> None:
-    # seems like hook isnt called on clients anyway (? not versed enought in sdk mods yet)
-    # making sure to avoid unknown territory in case it is for some people
-    if is_client():
+    # hook is only called on the server, so we broadcast the data to the clients in a coroutine
+    if DamageMeterState.is_paused:
         return
-
     # add tanked damage stats in the future
     # if obj.Class.Name == "WillowPlayerPawn":
     #     return
@@ -103,7 +113,7 @@ def took_damage_from_enemy(
         return
 
     player_name = instigator.PlayerReplicationInfo.PlayerName
-    new_stats = DamageMeterState.player_stats.copy()
+    new_stats = DamageMeterState.player_stats
 
     if player_name not in new_stats:
         new_stats[player_name] = {
@@ -117,9 +127,6 @@ def took_damage_from_enemy(
     damage_summary = args.Pipeline.DamageSummary
     new_stats[player_name]["damage"] += int(damage_summary.FinalDamage) + int(damage_summary.DamageDealtToShields)
 
-    # call the broadcast function so all clients get the new stats
-    store_stats(new_stats)
-
 
 # straight from SO @rtaft https://stackoverflow.com/a/45846841
 def human_format(num: float) -> str:
@@ -131,51 +138,77 @@ def human_format(num: float) -> str:
     return "{}{}".format("{:f}".format(num).rstrip("0").rstrip("."), ["", "K", "M", "B", "T", "Q", "E"][magnitude])
 
 
-@hook("WillowGame.WillowGameViewportClient:PostRender")
-def post_render(
-    obj: WillowGame.WillowGameViewportClient,
-    args: WillowGame.WillowGameViewportClient._PostRender.args,
-    __ret: WillowGame.WillowGameViewportClient._PostRender.ret,
-    __func: BoundFunction,
-) -> None:
-    if not DamageMeterState.is_logging:
+def coroutine_draw_meter() -> PostRenderCoroutine:
+    while True:
+        yield WaitUntil(lambda: get_pc().GetHUDMovie() is not None)
+        canvas = yield
+        if DamageMeterState.is_hidden:
+            continue
+
+        drawing.reset_state(canvas)
+        drawing.draw_background(drawing.gray_color_bg)
+
+        drawing.draw_text_new_line(TITLE, drawing.gold_color)
+
+        for player_name, stats in DamageMeterState.player_stats.items():
+            drawing.draw_hline_under_text(drawing.white_color)
+            player_class_attributes = ATTRIBUTES[stats["character_class"]]
+            drawing.draw_text_new_line(
+                player_name + " - " + player_class_attributes["display_name"] + ": " + human_format(stats["damage"]),
+                player_class_attributes["color"],
+            )
+
+
+# from slide mod by @juso40
+e_net_mode: WorldInfo.ENetMode = cast("WorldInfo.ENetMode", find_enum("ENetMode"))
+
+
+def is_client() -> bool:
+    return cast("WillowGameEngine", ENGINE).GetCurrentWorldInfo().NetMode == e_net_mode.NM_Client
+
+
+@broadcast.json_message
+def client_store_stats(stats: dict[str, PlayerStats]) -> None:
+    if not is_client():
         return
-
-    if args.Canvas is None:
-        return
-
-    drawing.draw_background(drawing.gray_color_bg)
-    drawing.reset_state(args.Canvas)
-
-    drawing.draw_text_new_line(TITLE, drawing.gold_color)
-    # font is monospaced, so this is just a random number
-    drawing.draw_line(len(TITLE) * 2, drawing.white_color)
-
-    for player_name, stats in DamageMeterState.player_stats.items():
-        player_class_attributes = ATTRIBUTES[stats["character_class"]]
-        drawing.draw_text_new_line(
-            player_name + " - " + player_class_attributes["display_name"] + ": " + human_format(stats["damage"]),
-            player_class_attributes["color"],
-        )
-    # cba to fix, but only now he knows the line count to know how large the bg should be
+    DamageMeterState.player_stats = stats
 
 
-@keybind("Show/Hide Meter", key="F10")
+def coroutine_send_stats_every_second() -> PostRenderCoroutine:
+    while True:
+        yield WaitForSeconds(1)
+        client_store_stats(DamageMeterState.player_stats)
+
+
+@keybind("Enable/Disable Meter", key="F10")
 def start_meter() -> None:
-    DamageMeterState.is_logging = not DamageMeterState.is_logging
     DamageMeterState.player_stats = {}
+    DamageMeterState.is_hidden = not DamageMeterState.is_hidden
+    DamageMeterState.is_paused = DamageMeterState.is_hidden
 
 
-@keybind("Reset Meter", key="P")
+@keybind("Reset Meter", key="O")
 def reset_meter() -> None:
     DamageMeterState.player_stats = {}
 
 
+@keybind("(Un)Pause Meter", key="P")
+def pause_meter() -> None:
+    DamageMeterState.is_paused = not DamageMeterState.is_paused
+
+
+def on_enable():
+    start_coroutine_post_render(coroutine_send_stats_every_second())
+    start_coroutine_post_render(coroutine_draw_meter())
+
+
 mod = build_mod(
-    keybinds=[start_meter, reset_meter],
-    hooks=[took_damage_from_enemy, post_render],
+    options=[defaultActive, drawing.opt_bg_opacity, drawing.opt_x_pos, drawing.opt_y_pos, drawing.opt_y_inc],
+    hooks=[took_damage_from_enemy, on_spawn],
+    on_enable=on_enable,
     coop_support=CoopSupport.RequiresAllPlayers,  # not all but atleast host
     supported_games=Game.BL2,
 )
+
 
 add_network_functions(mod)
--- DamageMeter/drawing.py
+++ DamageMeter/drawing.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from types import EllipsisType
 from typing import TYPE_CHECKING, cast
+from mods_base import options
 from unrealsdk import find_object, make_struct
 
 if TYPE_CHECKING:
@@ -19,6 +20,79 @@ else:
     make_struct_glow_info = make_struct_glow_info = make_struct
     make_struct_font_render_info = make_struct_font_render_info = make_struct
 
+
+# values that (potentially) changing every frame
+class DrawingState:
+    canvas: Canvas = None
+    screen_width: int = 1920
+    screen_height: int = 1080
+
+    # the max width/lines is the width/count of the last frame
+    running_max_width: int = 0
+    running_num_lines: int = 0
+    max_width: int = 0
+    max_lines: int = 0
+
+    text_height: int = 0
+    y_inc: int = 40
+
+    x_pos: int = 35
+    y_pos: int = 400
+    bg_padding_x: int = 10
+    bg_padding_y: int = 5
+    bg_opacity: int = 255
+
+
+def on_bg_opacity_change(option: options.SliderOption, value: int) -> None:
+    DrawingState.bg_opacity = value
+
+
+def on_x_pos_change(option: options.SliderOption, value: int) -> None:
+    DrawingState.x_pos = value
+
+
+def on_y_pos_change(option: options.SliderOption, value: int) -> None:
+    DrawingState.y_pos = value
+
+
+def on_y_inc_change(option: options.SliderOption, value: int) -> None:
+    DrawingState.y_inc = value
+
+
+opt_bg_opacity = options.SliderOption(
+    identifier="Background Opacity",
+    value=255,
+    min_value=0,
+    max_value=255,
+    description="The opacity of the background",
+    on_change=on_bg_opacity_change,
+)
+
+opt_x_pos = options.SliderOption(
+    identifier="X Position",
+    value=35,
+    min_value=0,
+    max_value=1720,
+    description="The x position of the damage meter. Default value is 35",
+    on_change=on_x_pos_change,
+)
+opt_y_pos = options.SliderOption(
+    identifier="Y Position",
+    value=400,
+    min_value=0,
+    max_value=1040,
+    description="The y position of the damage meter. Default value is 400",
+    on_change=on_y_pos_change,
+)
+
+opt_y_inc = options.SliderOption(
+    identifier="Y Increment",
+    value=40,
+    min_value=0,
+    max_value=100,
+    description="The distance between each line of text. Default value is 40",
+    on_change=on_y_inc_change,
+)
 # helper colors
 axton_green_color: Object.Color = make_struct_color("Color", R=0, G=100, B=0, A=255)
 maya_yellow_color: Object.Color = make_struct_color("Color", R=200, G=200, B=0, A=255)
@@ -27,20 +101,11 @@ zero_cyan_color: Object.Color = make_struct_color("Color", R=0, G=120, B=160, A=
 gaige_purple_color: Object.Color = make_struct_color("Color", R=100, G=10, B=130, A=255)
 krieg_red_color: Object.Color = make_struct_color("Color", R=100, G=10, B=0, A=255)
 
-
 # allow to change via options in the future
 gray_color_bg: Object.Color = make_struct_color("Color", R=125, G=125, B=125, A=255)
 black_color: Object.Color = make_struct_color("Color", R=0, G=0, B=0, A=255)
 white_color: Object.Color = make_struct_color("Color", R=255, G=255, B=255, A=255)
 gold_color: Object.Color = make_struct_color("Color", R=255, G=165, B=0, A=255)
-
-# allow to change via options in the future
-start_x: int = 600
-start_y: int = 35
-bg_padding: int = 10
-width: int = 200
-start_height: int = 0
-y_inc: int = 40
 
 
 # some stuff that DrawText needs for the out variable so we fill with dummy values
@@ -62,28 +127,46 @@ font_render_info = make_struct_font_render_info(
 )
 
 
-# values changing every frame
-class DrawingState:
-    canvas: Canvas = None
-    num_displayed_lines: int = 0
-    width: int = 0
-
-
 # has to be called every frame for other functions to work (weird setup, but alas)
 def reset_state(canvas: Canvas, font: Font = font_willowbody_18pt) -> None:
-    DrawingState.canvas = canvas
-    DrawingState.canvas.Font = font
-    DrawingState.num_displayed_lines = 0
-    DrawingState.width = 0
+    ds = DrawingState
+    ds.canvas = canvas
+    ds.canvas.Font = font
+
+    # magic numbers that are <= the size of the box
+    opt_x_pos.max_value = canvas.SizeX - 200
+    opt_y_pos.max_value = canvas.SizeY - 50
+
+    # update the max width/lines of the last frame
+    ds.max_width = ds.running_max_width
+    ds.running_max_width = 0
+    ds.max_lines = ds.running_num_lines
+    ds.running_num_lines = 0
 
 
-def get_text_width(text: str) -> int:
+def get_text_size(text: str) -> tuple[int, int]:
     values = DrawingState.canvas.TextSize(text, 0, 0)
-    # super hacky but SOMETIMES the return value is just an (float, float) tuple without an ellipsis at the start
-    for value in values:
-        if isinstance(value, EllipsisType):
-            continue
-        return value
+    # SUPER hacky but SOMETIMES the return value is just (float, float) without an ellipsis at the start
+    return (values[-2], values[-1])
+
+
+def draw_text(
+    text: str,
+    color: Object.Color,
+    *,
+    x: int = 0,
+    y: int = 0,
+) -> None:
+    ds = DrawingState
+    width, height = get_text_size(text)
+    if width > ds.running_max_width:
+        ds.running_max_width = width
+    ds.text_height = height
+
+    canvas = ds.canvas
+    canvas.SetPos(x, y)
+    canvas.SetDrawColorStruct(color)
+    canvas.DrawText(text, True, 1, 1, font_render_info)
 
 
 def draw_text_new_line(text: str, color: Object.Color) -> None:
@@ -91,35 +174,10 @@ def draw_text_new_line(text: str, color: Object.Color) -> None:
     draw_text(
         text=text,
         color=color,
-        y=start_y + ds.num_displayed_lines * y_inc,
+        x=ds.x_pos,
+        y=ds.y_pos + ds.running_num_lines * ds.y_inc,
     )
-    ds.num_displayed_lines += 1
-
-
-def draw_text(
-    text: str,
-    color: Object.Color,
-    *,
-    x: int = start_x,
-    y: int = start_y,
-) -> None:
-    width = get_text_width(text)
-    if width > DrawingState.width:
-        DrawingState.width = width
-
-    canvas = DrawingState.canvas
-    canvas.SetPos(x, y)
-    canvas.SetDrawColorStruct(color)
-    canvas.DrawText(text, True, 1, 1, font_render_info)
-
-
-def draw_line(length: int, color: Object.Color) -> None:
-    ds = DrawingState
-    draw_text(
-        text="-" * length,
-        color=color,
-        y=start_y + ds.num_displayed_lines * y_inc - y_inc / 2,
-    )
+    ds.running_num_lines += 1
 
 
 def draw_rectangle(x: int, y: int, width: int, height: int, color: Object.Color) -> None:
@@ -130,13 +188,26 @@ def draw_rectangle(x: int, y: int, width: int, height: int, color: Object.Color)
     canvas.DrawRect(width, height, tex)
 
 
+def draw_hline_under_text(color: Object.Color, thickness: int = 1) -> None:
+    ds = DrawingState
+    draw_rectangle(
+        x=ds.x_pos - ds.bg_padding_x,
+        y=ds.y_pos + ds.running_num_lines * ds.y_inc - (ds.y_inc - ds.text_height) // 2,
+        width=DrawingState.max_width + 2 * ds.bg_padding_x,
+        height=thickness,
+        color=color,
+    )
+
+
 def draw_background(color: Object.Color) -> None:
     if DrawingState.canvas is None:
         return
+    ds = DrawingState
+    color.A = ds.bg_opacity
     draw_rectangle(
-        start_x - bg_padding,
-        start_y - bg_padding,
-        DrawingState.width + bg_padding * 2,
-        start_height + DrawingState.num_displayed_lines * y_inc + bg_padding * 2,
-        color,
+        x=ds.x_pos - ds.bg_padding_x,
+        y=ds.y_pos - ds.bg_padding_y,
+        width=DrawingState.max_width + ds.bg_padding_x * 2,
+        height=DrawingState.max_lines * ds.y_inc + ds.bg_padding_y * 2,
+        color=color,
     )
